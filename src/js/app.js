9a471998598ae803ca0f0df688bd3c53bd26000e
kvitka <kvitka@kvitkas-MacBook-Pro.local>
2016-01-06 19:44:55 +0200

    add fotorama

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,13 @@
 $(function() {
-  console.log('rozetka');
+  $('.fotorama').fotorama({
+    width: 875,
+    height: 290,
+    maxwidth: '100%',
+    //ratio: 16/9,
+    allowfullscreen: true,
+    nav: 'dots',
+    //autoplay: true,
+  });
 });
 
 
