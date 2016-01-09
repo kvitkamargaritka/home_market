3dfeb58a06fd8b758d991588a0dc3f317f9c19d7
kvitka <kvitka@kvitkas-MacBook-Pro.local>
2016-01-09 19:23:56 +0200

    add carousel

--- build/js/app.js
+++ build/js/app.js
@@ -1,13 +1,38 @@
 $(function() {
+/** Fotorama **/
+
   $('.fotorama').fotorama({
     width: 875,
     height: 290,
     maxwidth: '100%',
     //ratio: 16/9,
-    allowfullscreen: true,
+    //allowfullscreen: true,
     nav: 'dots',
     //autoplay: true,
   });
+/** end Fotorama **/
+
+/** slick **/
+
+  $('.carousel').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: true,
+  });
+
+/** end slick **/
+
+  var elements = $('.general-menu__link');
+
+  elements.each(function (index, el) {
+    $(el).hover(function () {
+      //$(this).parent().find('.content').toggle();
+      $(this).parent().find('.general-menu__dropdown').fadeToggle();
+    })
+  });
+
+
 });
 
 
